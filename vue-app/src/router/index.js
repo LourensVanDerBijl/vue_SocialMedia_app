1b812499c993fe3a3e3bb73a2183b626d1a60862
louis3546 <lourensvdbijl@gmail.com>
2021-05-31 12:21:38 +0200

    update 3

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -1,25 +1,39 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Dashboard from '../views/Dashboard.vue'
+import { auth } from '../firebase'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Dashboard',
+        component: Dashboard,
+        meta: {
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () =>
+            import ( /* webpackChunkName: "login" */ '../views/Login.vue')
+    },
+    {
+        path: '/settings',
+        name: 'settings',
+        component: () =>
+            import ( /* webpackChunkName: "settings" */ '../views/Settings.vue'),
+        meta: {
+            requiresAuth: true
+        }
+    }
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+router.beforeEach((to, from, next) => {
+    const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
 
-export default router
+    if (requiresAuth && !auth.currentUser) {
+        next('/login')
+    } else {
+        next()
+    }
+})
